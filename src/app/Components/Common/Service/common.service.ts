7be8688713d018beba5699e6cc7a641279b69bb6
veysel <vinanc72@gmail.com>
2017-04-15 18:38:51 +0300

    Used service notification

--- src/app/Components/Common/Service/common.service.ts
+++ src/app/Components/Common/Service/common.service.ts
@@ -4,11 +4,21 @@ import { Observable } from 'rxjs/Rx';
 
 import 'rxjs/add/operator/map';
 
+import { ViNotificationComponent } from '../../../Tools/Components/ViNotification/vi.notification.component';
+
 @Injectable()
 export class CommonService {
 
+    constructor(
+        private _viNotificationComponent: ViNotificationComponent
+    ) { }
+
     public getUserListApiUrl() {
         return "https://api.myjson.com/bins/zo8tf";
+    }
+
+    public showNotification(titleText: string, contentText: string) {
+        this._viNotificationComponent.showNotification(titleText, contentText);
     }
 
 }
--- src/app/Components/Default/default.component.ts
+++ src/app/Components/Default/default.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CommonService } from '../Common/Service/common.service';
+
 @Component({
     selector: 'default',
     templateUrl: 'default.template.html'
 })
 export class DefaultComponent implements OnInit {
-    constructor() { }
+    constructor(
+        private _commonService: CommonService
+    ) { }
 
     ngOnInit() { }
+
+    private showNotification() {
+        this._commonService.showNotification("Test Başlık", "Test İçerik");
+    }
+
 }
