91ff501297cbb1ad9bdc2d1f6852a018f83aaeb3
veysel <vinanc72@gmail.com>
2017-04-16 02:21:30 +0300

    Test component clear

--- src/app/Components/Default/default.component.ts
+++ src/app/Components/Default/default.component.ts
@@ -1,24 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 import { CommonService } from '../Common/Service/common.service';
-import { ViNotificationComponent } from '../../Tools/Components/ViNotification/vi.notification.component';
 
 @Component({
     selector: 'default',
-    templateUrl: 'default.template.html',
-    providers: [ViNotificationComponent]
+    templateUrl: 'default.template.html'
 })
 export class DefaultComponent implements OnInit {
     constructor(
-        private _commonService: CommonService,
-        private _viNotificationComponent: ViNotificationComponent
+        private _commonService: CommonService
     ) { }
 
     ngOnInit() { }
-
-    private showNotification() {
-        // this._commonService.showNotification("Test Başlık", "Test İçerik");
-        this._viNotificationComponent.showNotification("Test Başlık", "Test İçerik");
-    }
 
 }
