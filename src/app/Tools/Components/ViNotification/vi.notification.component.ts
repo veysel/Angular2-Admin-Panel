c09f1b85d3e8172d8e38d0ca2d6d1fdbb1c90b05
veysel <vinanc72@gmail.com>
2017-04-15 18:19:04 +0300

    Notification content changed

--- src/app/Tools/Components/ViNotification/vi.notification.component.ts
+++ src/app/Tools/Components/ViNotification/vi.notification.component.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ViNotificationComponent implements OnInit {
+    private contentText = "";
+    private titleText = "";
+    private notificationState = false;
+
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() {
+
+    }
+
+    private close() {
+        this.notificationState = false;
+    }
+
+    private autoClose() {
+        setTimeout(this.close(), 2000);
+    }
+
+    public showNotification(titleText: string, contentText: string) {
+        this.titleText = titleText;
+        this.contentText = contentText;
+        this.notificationState = true;
+        this.autoClose();
+    }
 }
