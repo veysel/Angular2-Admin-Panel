40d114f41dfe4a53a7001260e9cec958e5d82063
veysel <vinanc72@gmail.com>
2017-04-17 21:31:56 +0300

    Route link updated

--- src/app/Components/Main/Panel/Menu/MenuTop/menu.top.component.ts
+++ src/app/Components/Main/Panel/Menu/MenuTop/menu.top.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'menu-top',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class MenuTopComponent implements OnInit {
     public searchText = "";
 
-    constructor() { }
+    constructor(
+        private _router: Router
+    ) { }
 
     ngOnInit() { }
+
+    private goToSearch() {
+        this._router.navigate(['/main/panel/page/search', this.searchText]);
+    }
 }
