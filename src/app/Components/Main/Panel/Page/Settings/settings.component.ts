e570a9efc5d7e34867707e4a059e455e3130d70b
veysel <vinanc72@gmail.com>
2017-04-22 23:46:26 +0300

    Settings content changed

--- src/app/Components/Main/Panel/Page/Settings/settings.component.ts
+++ src/app/Components/Main/Panel/Page/Settings/settings.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserModel } from '../../../Login/Model/user.model';
 
 import { UserService } from '../../../Login/Service/user.service';
+import { StorageService } from '../../../../Storage/Service/storage.service';
 
 @Component({
     selector: 'settings',
@@ -10,14 +11,18 @@ import { UserService } from '../../../Login/Service/user.service';
 })
 export class SettingsComponent implements OnInit {
     private userList: Array<UserModel>;
+    private loginUser: UserModel;
 
     constructor(
-        private _userService: UserService
+        private _userService: UserService,
+        private _storageService: StorageService
     ) {
         this.userList = new Array<UserModel>();
+        this.loginUser = new UserModel();
     }
 
     ngOnInit() {
         this._userService.getUserList().subscribe(res => { this.userList = res.list; });
+        this.loginUser = this._storageService.getStorage();
     }
 }
