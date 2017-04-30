d48b8bc665d972367f1ec8409e28ce1834974a95
veysel <vinanc72@gmail.com>
2017-04-30 23:22:53 +0300

    Menu left updated

--- src/app/Components/Main/Panel/Menu/MenuLeft/menu.left.component.ts
+++ src/app/Components/Main/Panel/Menu/MenuLeft/menu.left.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { StorageService } from '../../../../Storage/Service/storage.service';
+
+import { UserModel } from '../../../Login/Model/user.model';
+
 @Component({
     selector: 'menu-left',
     templateUrl: 'menu.left.template.html'
 })
 export class MenuLeftComponent implements OnInit {
+    private userModel: UserModel;
     private searchText = "";
 
     constructor(
         private _router: Router,
-    ) { }
+        private _storageService: StorageService
+    ) {
+
+    }
 
     ngOnInit() {
-
+        this.userModel = this._storageService.getStorage();
     }
 
     private goToSearch() {
