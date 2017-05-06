8249a183213cbf1d18e3939c8933ed380104d155
veysel <vinanc72@gmail.com>
2017-05-06 19:32:42 +0300

    Custom notification changed

--- src/app/Tools/Components/ViNotification/vi.notification.component.ts
+++ src/app/Tools/Components/ViNotification/vi.notification.component.ts
@@ -8,31 +8,15 @@ import { Component, OnInit } from '@angular/core';
 
 export class ViNotificationComponent implements OnInit {
     private contentText: string;
-    private titleText: string;
     private notificationState: boolean;
 
-    constructor() {
-        this.close();
-    }
-
     ngOnInit() {
-
-    }
-
-    private close() {
-        this.notificationState = false;
-        this.titleText = "";
-        this.contentText = "";
-    }
-
-    private autoClose() {
-        setTimeout(x => { this.close(); }, 1000);
-    }
-
-    public showNotification(titleText: string, contentText: string) {
         this.notificationState = true;
-        this.titleText = titleText;
+        this.contentText = "Kayıt Başarılı";
+    }
+
+    public showNotification(contentText: string) {
+        this.notificationState = true;
         this.contentText = contentText;
-        this.autoClose();
     }
 }
