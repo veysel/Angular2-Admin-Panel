31d4a6f044b01f6a6fce75c86dbfb1db060de71e
veysel <vinanc72@gmail.com>
2017-05-07 18:47:21 +0300

    Custom modals added

---
+++ src/app/Tools/Components/ViModals/ViNotification/vi.modals.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'vi-modals',
+    templateUrl: './vi.modals.template.html'
+})
+
+export class ViModalsComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() {
+    }
+}
