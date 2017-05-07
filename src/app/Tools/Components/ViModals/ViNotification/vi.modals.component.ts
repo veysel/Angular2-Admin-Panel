14de3ffb71fa43ade74b7aacc71ec6bd60055ecf
veysel <vinanc72@gmail.com>
2017-05-07 18:51:02 +0300

    Modals content changed

--- src/app/Tools/Components/ViModals/ViNotification/vi.modals.component.ts
+++ src/app/Tools/Components/ViModals/ViNotification/vi.modals.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ViModalsComponent implements OnInit {
+    private modalsTitle = "";
+    private modalsBody = "";
+
     constructor() { }
 
     ngOnInit() {
