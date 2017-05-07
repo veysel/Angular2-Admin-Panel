9ac2d2f6e059fede95e5c6f001e7f51187befa30
veysel <vinanc72@gmail.com>
2017-05-07 18:51:44 +0300

    Modals component code refactor

--- src/app/Tools/Components/ViModals/ViNotification/vi.modals.component.ts
+++ src/app/Tools/Components/ViModals/ViNotification/vi.modals.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ViModalsComponent implements OnInit {
-    private modalsTitle = "";
-    private modalsBody = "";
+    private modalsTitle: string;
+    private modalsBody: string;
 
     constructor() { }
 
